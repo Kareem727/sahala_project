daf3b8721f9b7a3cee1c0c8dabcc9413daae651a
Kareem727 <kareem.ga.13@gmail.com>
2022-02-25 17:39:54 +0200

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,40 +5,41 @@ const Footer = () =>{
         <bdo dir='rtl'>
             
 <div className={stylesss.container}>
+
     <div className={stylesss.container3}>
-<div className={stylesss.gridss}>
+    <div className={stylesss.gridss}>
     <img src={logo}></img>
     <p>قم بتصفح مختلف الأقسام و المنتجات و قم بتواصل مع مختلف مزودي الخدمات للتفاوض على الأسعار و لشراء المنتج</p>
-</div>
-</div>
+    </div>
+    </div>
+
+
 <div className={stylesss.container2}>
+    <div className={stylesss.footerBlock3}>
 <div className={stylesss.gridss}>
-    <p>خرائط المواقع</p>
-      
+    <p>خرائط المواقع</p>  
         <li className={stylesss.mgtLi}>الرئيسية</li>
-        <li>المزايا</li>
-    
+        <li>المزايا</li>  
 </div>
 <div className={stylesss.gridss}>
-    <p>المزايا</p>
-     
+        <p>المزايا</p>
         <li className={stylesss.mgtLi}>مختلف المتاجر</li>
         <li>تواصل المزودين</li>
         <li>الحصول على إشعار</li>
-    
-</div>   
-<div className={stylesss.gridss2}>
-    <p className={stylesss.marb}>تابعنا</p>
-     
+</div>
+</div>  
+<div className={stylesss.gridss}> 
+    <p>تابعنا</p>
     <div className={stylesss.icons}>
-    <i className='fa fa-linkedin'></i>      
-    <i className="fa fa-twitter"></i>
-    <i className="fa fa-facebook-f"></i>
+    <div><i className='fa fa-linkedin'></i>   </div>   
+    <div><i className="fa fa-twitter"></i></div>
+    <div><i className="fa fa-facebook-f"></i></div>
     </div>
+</div>
+</div>
 
-    
-</div>
-</div>
+
+
 </div>
         </bdo>
     );
